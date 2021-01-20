3229ff9ef2fd9c793dc1d0393e498a0076ebdcdf
Alok Raj <alokr417@gmail.com>
2021-01-20 09:17:20 +0530

    project structure added + firebase config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAmFtcNh09pKD0wd63NBo8OFknxj7Y-D3Q",
+    authDomain: "github722.firebaseapp.com",
+    projectId: "github722",
+    storageBucket: "github722.appspot.com",
+    messagingSenderId: "31509824010",
+    appId: "1:31509824010:web:f6a8be445c881bd35f55f3",
+    measurementId: "G-XG59D5PS4D"
+  }
 };
