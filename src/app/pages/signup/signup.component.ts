0abb9f5548dfa46af8f3b3106770f5345b3dc786
Alok Raj <alokr417@gmail.com>
2021-01-20 12:19:06 +0530

    signup component developed

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    const { email, password } = f.form.value;
+    this.auth
+      .signUp(email, password)
+      .then((res) => {
+        this.router.navigateByUrl('/');
+        this.toastr.success('Signup Success');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        this.toastr.error('Singup failed');
+      });
   }
 
 }
